91e42ec4eb2e692edf6218ce639d978b5dc6c889
Yousef eslam <yousefeslam214@gmail.com>
2024-09-02 10:07:52 +0300

    update

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Module of Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """ Class to manage the API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if not( path[ len(path) - 1] == '/'):
+            path += '/'
+        if path not in excluded_paths:
+            return True
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that handles authorization header """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates current user """
+        return None
