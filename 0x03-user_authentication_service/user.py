bfb520820ba39870c1fe12173c30d98bac555672
Yousef eslam <yousefeslam214@gmail.com>
2024-09-10 12:00:42 +0300

    update

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Class User for ORM"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class User(Base):
+    """Representation of a user """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+    # def __repr__(self):
+    #    return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+    #                         self.name, self.fullname, self.nickname)
